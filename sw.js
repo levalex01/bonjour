3c7405edb32d61f89d7b67102e385a1d2c46d3f0
levalex01 <101523166+levalex01@users.noreply.github.com>
2025-06-04 16:09:02 -0400

    web app file

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("bonjour-cache").then((cache) => {
+      return cache.addAll(["index.html", "style.css", "manifest.json", "icon-192.png", "icon-512.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
